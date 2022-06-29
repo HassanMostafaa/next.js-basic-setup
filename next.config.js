ba81060134be5ffdea54d563f4b5f89cb8b28193
Hassan Mostafa <48831222+HassanMostafaa@users.noreply.github.com>
2022-06-29 04:28:16 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  env: {
-    MONGO_CON_STR: `mongodb+srv://hassanAdmin:123321123321@basic-notes-cluster.ad4bq.mongodb.net/mainNotesDatabase?retryWrites=true&w=majority`,
-  },
-};
-
-module.exports = nextConfig;
